0eb9844b3283a1db4ff16f84d4bfc1a69c4be760
Diego Vega <83849430+diegovega223@users.noreply.github.com>
2024-06-12 18:41:13 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ function install_php {
     echo "Instalando PHP..."
     sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
     sudo yum-config-manager --enable remi-php82
-    sudo yum install -y php php-cli php-fpm php-mysqlnd php-xml php-mbstring php-zip php-devel php-intl php-soap
+    sudo yum install -y php php-cli php-fpm php-mysqlnd php-xml php-mbstring php-zip php-devel php-intl php-soap php-ldap
     echo "PHP instalado."
 }
 
@@ -58,7 +58,7 @@ function install_docker_compose {
 
 function install_domain_dependencies {
     echo "Instalando dependencias necesarias para unir al dominio..."
-    sudo yum install -y realmd sssd adcli samba-common samba-common-tools oddjob oddjob-mkhomedir krb5-workstation
+    sudo yum install -y realmd sssd adcli samba-common samba-common-tools oddjob oddjob-mkhomedir krb5-workstation openldap-clients
     echo "Dependencias para unir al dominio instaladas."
 }
 
