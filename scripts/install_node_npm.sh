ac3a4841d36a0adcb561c5bf13905e13d6e847b7
Mateo Sosa <mateesosar@gmail.com>
2024-07-04 20:25:51 -0300

    Para descargar ultima version de node.js

--- scripts/install_node_npm.sh
+++ scripts/install_node_npm.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
-INSTALL_DIR=~/opt/node
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-INSTALL_DIR=/opt/node
-
-echo "Instalando Node.js versión 20.13.1 en $INSTALL_DIR"
-
-wget https://unofficial-builds.nodejs.org/download/release/v20.13.1/node-v20.13.1-linux-x64-glibc-217.tar.gz -P "$SCRIPT_DIR"
-
-sudo tar zxf "$SCRIPT_DIR/node-v20.13.1-linux-x64-glibc-217.tar.gz" -C /opt
-
-if [ -d "$INSTALL_DIR" ]; then
-    sudo mv "$INSTALL_DIR" "$INSTALL_DIR.old"
+if ! command -v curl &> /dev/null; then
+    sudo yum install -y curl
 fi
 
-sudo mv "/opt/node-v20.13.1-linux-x64-glibc-217" "$INSTALL_DIR"
 
-export PATH=$INSTALL_DIR/bin:$PATH
+sudo yum install epel-release -y
+
+curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo bash -
+sudo yum install -y nodejs
 
 node -v
 
-if ! sudo grep -q 'export PATH=/opt/node/bin:$PATH' /etc/profile; then
-    echo 'export PATH=/opt/node/bin:$PATH' | sudo tee -a /etc/profile
-    source /etc/profile
-fi
+npm -v
+
 
 echo "Node.js y npm instalados."
