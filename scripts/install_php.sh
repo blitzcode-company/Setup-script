abc0f072bfbc001ad1fd11340370011efa93ec9b
Mateo Sosa <mateesosar@gmail.com>
2024-07-04 19:45:14 -0300

    Para descargar ultima version de node.js

--- scripts/install_php.sh
+++ scripts/install_php.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 echo "Instalando PHP..."
-sed -i 's/mirror.centos.org/vault.centos.org/g' /etc/yum.repos.d/*.repo
-sed -i 's/^#.*baseurl=http/baseurl=http/g' /etc/yum.repos.d/*.repo
-sed -i 's/^mirrorlist=http/#mirrorlist=http/g' /etc/yum.repos.d/*.repo
 
 sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum-config-manager --enable remi-php82
