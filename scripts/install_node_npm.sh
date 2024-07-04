dc54888f97f2dddb72ccaf8936993bec66cca798
Mateo Sosa <mateesosar@gmail.com>
2024-07-04 20:09:04 -0300

    Para descargar ultima version de node.js

--- scripts/install_node_npm.sh
+++ scripts/install_node_npm.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 echo "Instalando Node.js y npm..."  
-git clone https://github.com/nodejs/node
-cd node
-
-git checkout v20.11.0
-
-./configure
-make -j4
-
-sudo make install
-
+wget https://unofficial-builds.nodejs.org/download/release/v20.13.1/node-v20.13.1-linux-x64-glibc-217.tar.gz
+tar zxf node-v20.13.1-linux-x64-glibc-217.tar.gz
+mv ~/opt/node ~/opt/node.old
+cp -r node-v20.13.1-linux-x64-glibc-217 ~/opt/node
 node -v
-npm -v
 
 echo "Node.js y npm instalados."
