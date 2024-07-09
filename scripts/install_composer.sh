95d5ec7e97e689aeeda3bf776ec1881fcaafd932
Diego Vega <83849430+diegovega223@users.noreply.github.com>
2024-07-09 19:05:55 -0300

    Update install_composer.sh

--- scripts/install_composer.sh
+++ scripts/install_composer.sh
@@ -2,6 +2,6 @@
 
 echo "Instalando Composer..."
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 php -r "unlink('composer-setup.php');"
 echo "Composer instalado."
