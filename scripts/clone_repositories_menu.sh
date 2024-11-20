1e47dc8d06957cbb82bbb0df73550062c897d1ab
MateeSR <92068513+MateeSR@users.noreply.github.com>
2024-11-19 21:03:43 -0300

    Update clone_repositories_menu.sh

--- scripts/clone_repositories_menu.sh
+++ scripts/clone_repositories_menu.sh
@@ -10,10 +10,13 @@ API_PAGOS_REPO="https://github.com/blitzcode-company/api-pagos.git"
 MYSQL_MASTER_REPO="https://github.com/blitzcode-company/Mysql-master.git"
 MYSQL_SLAVE_REPO="https://github.com/blitzcode-company/Mysql-slave.git"
 MONITOREO_REPO="https://github.com/blitzcode-company/Monitoreo.git"
+MODERADORES_REPO="https://github.com/blitzcode-company/Blitzvideo-ApiModeradores.git"
 DOCKER_REPO="https://github.com/blitzcode-company/Docker.git"
-
+MODERADORES_VISUALIZER_REPO="https://github.com/blitzcode-company/Blitzvideo-ModeradoresVisualizer.git"
 
 BLITZVIDEO_API_DIR="Blitzvideo-api"
+MODERADORES_API_DIR="Blitzvideo-ApiModeradores"
+MODERADORES_VISUALIZER_DIR="Blitzvideo-ModeradoresVisualizer"
 OAUTH_API_DIR="Oauth-api"
 BLITZVIDEO_AUTH_DIR="Blitzvideo-Auth"
 BLITZVIDEO_VISUALIZER_DIR="Blitzvideo-Visualizer"
@@ -41,6 +44,23 @@ clone_and_setup_blitzvideo_api() {
     if [ ! -d "$BLITZVIDEO_API_DIR" ]; then
         git clone $BLITZVIDEO_API_REPO &&
         cd $BLITZVIDEO_API_DIR &&
+        read -p "¿Quieres ejecutar 'composer install' y 'cp .env.example .env'? (y/n): " install_option
+        if [[ "$install_option" == "y" || "$install_option" == "Y" ]]; then
+            composer install
+            cp .env.example .env
+            php artisan key:generate
+        fi
+        cd ..
+    else
+        echo "------------------------------------"
+        echo "El repositorio ya ha sido clonado."
+        echo "------------------------------------"
+    fi
+}
+clone_and_setup_blitzvideo_api_moderadores() {
+    if [ ! -d "$MODERADORES_API_DIR" ]; then
+        git clone $MODERADORES_REPO &&
+        cd $MODERADORES_API_DIR &&
         read -p "¿Quieres ejecutar 'composer install' y 'cp .env.example .env'? (y/n): " install_option
         if [[ "$install_option" == "y" || "$install_option" == "Y" ]]; then
             composer install
@@ -142,6 +162,22 @@ clone_and_setup_blitzvideo_visualizer() {
     fi
 }
 
+clone_and_setup_blitzvideo_moderadores_visualizer() {
+    if [ ! -d "$MODERADORES_VISUALIZER_DIR" ]; then
+        git clone $MODERADORES_VISUALIZER_REPO &&
+        cd $MODERADORES_VISUALIZER_DIR &&
+        read -p "¿Quieres ejecutar 'npm install'?' (y/n): " install_option
+        if [[ "$install_option" == "y" || "$install_option" == "Y" ]]; then
+            npm install
+        fi
+        cd ..
+    else
+        echo "------------------------------------"
+        echo "El repositorio ya ha sido clonado."
+        echo "------------------------------------"
+    fi
+}
+
 clone_and_setup_blitzvideo_creadores() {
     if [ ! -d "$BLITZVIDEO_CREADORES_DIR" ]; then
         git clone $BLITZVIDEO_CREADORES_REPO &&
@@ -162,15 +198,17 @@ while true; do
     echo "Seleccione un repositorio para clonar:"
     echo "1. Blitzvideo-api (Backend)"
     echo "2. Oauth-api (Backend)"
-    echo "3. Blitzvideo-Auth (Frontend)"
-    echo "4. Blitzvideo-Visualizer (Frontend)"
-    echo "5. Blitzvideo-Creadores (Frontend)"
-    echo "6. Backoffice"
-    echo "7. Api-pagos (Backend)"
-    echo "8. Mysql-master (Base de Datos)"
-    echo "9. Mysql-slave (Base de Datos)"
-    echo "10. Monitoreo (Monitoreo)"
-    echo "11. Docker (Dockerfiles y docker-compose)"
+    echo "3. Moderadores-api (Backend)"
+    echo "4. Blitzvideo-Auth (Frontend)"
+    echo "5. Blitzvideo-Visualizer (Frontend)"
+    echo "6. Blitzvideo-Creadores (Frontend)"
+    echo "7. Blitzvideo-Moderadores (Frontend)"
+    echo "8. Backoffice"
+    echo "9. Api-pagos (Backend)"
+    echo "10. Mysql-master (Base de Datos)"
+    echo "11. Mysql-slave (Base de Datos)"
+    echo "12. Monitoreo (Monitoreo)"
+    echo "13. Docker (Dockerfiles y docker-compose)"
     echo "0. Volver al menú principal"
     read -p "Opción: " repo_option
 
@@ -178,15 +216,17 @@ while true; do
         0) break ;;
         1) clone_and_setup_blitzvideo_api ;;
         2) clone_and_setup_oauth_api ;;
-        3) clone_and_setup_blitzvideo_auth ;;
-        4) clone_and_setup_blitzvideo_visualizer ;;
-        5) clone_and_setup_blitzvideo_creadores ;;
-        6) clone_and_setup_backoffice ;;
-        7) clone_and_setup_api_pagos ;;
-        8) git clone $MYSQL_MASTER_REPO ;;
-        9) git clone $MYSQL_SLAVE_REPO ;;
-        10) git clone $MONITOREO_REPO ;;
-        11) clone_and_move_docker ;;
+        3) clone_and_setup_blitzvideo_api_moderadores;;
+        4) clone_and_setup_blitzvideo_auth ;;
+        5) clone_and_setup_blitzvideo_visualizer ;;
+        6) clone_and_setup_blitzvideo_creadores ;;
+        7) clone_and_setup_blitzvideo_moderadores_visualizer ;;
+        8) clone_and_setup_backoffice ;;
+        9) clone_and_setup_api_pagos ;;
+        10) git clone $MYSQL_MASTER_REPO ;;
+        11) git clone $MYSQL_SLAVE_REPO ;;
+        12) git clone $MONITOREO_REPO ;;
+        13) clone_and_move_docker ;;
         *) echo "Opción inválida. Inténtelo de nuevo." ;;
     esac
 done
