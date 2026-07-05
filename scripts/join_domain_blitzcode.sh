7705791669eacc224fdfd5b8f123263f52331801
Diego Vega <83849430+diegovega223@users.noreply.github.com>
2026-07-05 00:56:28 -0300

    Change network adapter from Bridge to NAT

--- scripts/join_domain_blitzcode.sh
+++ scripts/join_domain_blitzcode.sh
@@ -7,7 +7,7 @@ DC_IP="192.168.1.100"
 
 echo "=== 1. Optimizando configuración de Red y DNS ==="
 
-echo "Ajustando enp0s3 (Internet - Adaptador Puente)..."
+echo "Ajustando enp0s3 (Internet - NAT)..."
 
 sudo nmcli connection modify enp0s3 ipv4.dns "$DC_IP, 8.8.8.8"
 sudo nmcli connection modify enp0s3 ipv4.dns-search "$DOMAIN"
@@ -20,7 +20,6 @@ sudo nmcli connection modify enp0s8 ipv4.dns-search "$DOMAIN"
 sudo nmcli connection up enp0s8
 
 echo "=== 2. Forzando resolución local de IP para el Dominio ==="
-
 sudo sed -i "/$DOMAIN/d" /etc/hosts || true
 echo "$DC_IP $DOMAIN" | sudo tee -a /etc/hosts
 
@@ -46,7 +45,6 @@ sudo systemctl enable sssd oddjobd
 sudo systemctl start sssd oddjobd
 
 echo "=== 8. Configurando creación automática de Homes (authselect) ==="
-
 sudo authselect enable-feature with-mkhomedir
 sudo systemctl restart oddjobd
 
